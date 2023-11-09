e90b657b7a62b03385c744775ed7a4b137a18ffc
Pencho Ivanov <penchoivanov@Penchos-Air.home>
2023-11-09 17:12:12 +0100

    last changes

--- js/script.js
+++ js/script.js
@@ -5,20 +5,35 @@ window.onload = function () {
     const restartBtn = document.getElementById('restart-btn');
     
 
+    // beginning btn //
+
     const openingScreen = document.getElementById('opening-screen');
     const instruction = document.getElementById('instructions');
+    const gameContainer = document.getElementById('game-container');
 
+    // start btn - to instruction btn
     startBtn.addEventListener('click', function() {
         console.log('click')
         openingScreen.style.display = "none";
         instruction.style.display = "flex";
     } )
 
+    // instruction btn - to the game container
     startGameBtn.addEventListener('click', function(){
         console.log('click2')
-        startGameBtn.style.display = "none";
-        
+        instruction.style.display  = "none";
+        gameContainer.style.display = "block";
     })
+
+    //restart btn-refresh page 
+    restartBtn.addEventListener('click', function(){
+        restartGame()
+    })
+
+    function restartGame() {
+        location.reload();
+    }
+
 
 };
 
