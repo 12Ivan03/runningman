725915a5daa84ca3ce8601069888fb6731e498ea
Vijay Bhatt <36194921+ttahb@users.noreply.github.com>
2023-11-10 23:41:05 +0530

    Feature/runningman moves (#3)
    * test commit
    
    * code formatting, and tried to add something on second screen
    
    * code done with a btn JS'
    
    * added runnign man images and game screen
    
    * fix man
    
    * last changes
    
    * something
    
    * running man png
    
    * fixed conflict
    
    * running man - image
    
    * removed unwanted images
    
    * first js commit
    
    * roar strip completed
    
    * initialized player object
    
    * initialized player object
    
    * player jump
    
    * move player and jump
    
    * player moves and jumps
    
    ---------
    
    Co-authored-by: Pencho Ivanov <penchoivanov@Penchos-Air.home>
    Co-authored-by: Pencho Ivanov <penchoivanov@Penchos-MacBook-Air.local>

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     const startBtn = document.getElementById('start');
     const startGameBtn = document.getElementById('start-game');
-    const nextLevelBtn = document.getElementById('next-level');
+    // const nextLevelBtn = document.getElementById('next-level');
     const restartBtn = document.getElementById('restart-btn');
     
 
@@ -11,18 +11,19 @@ window.onload = function () {
     const instruction = document.getElementById('instructions');
     const gameContainer = document.getElementById('game-container');
 
+    let game;
+
     // start btn - to instruction btn
     startBtn.addEventListener('click', function() {
-        console.log('click')
+        console.log('click');
         openingScreen.style.display = "none";
         instruction.style.display = "flex";
     } )
 
     // instruction btn - to the game container
     startGameBtn.addEventListener('click', function(){
-        console.log('click2')
-        instruction.style.display  = "none";
-        gameContainer.style.display = "block";
+        game = new Game();
+        game.start();
     })
 
     //restart btn-refresh page 
@@ -34,6 +35,62 @@ window.onload = function () {
         location.reload();
     }
 
+    // Function to handle the jump
+    function jump(player) {
+        console.log("entered jump", player);
+        player.element.style.transform = 'translateY(-225px)';
+        setTimeout(() => {
+            player.element.style.transform = 'translateY(-225px)';
+            player.element.style.transform = 'translateY(0)';
+        }, 300);
+    }
+
+      
+    function handleKeydown(event) {
+        const code = event.code;
+        console.log("event", event);
+        console.log("code", code)
+        const possibleKeystrokes = [
+          "ArrowLeft",
+          "ArrowUp",
+          "ArrowRight",
+          "ArrowDown",
+          "c",
+          "Space" //space
+        ];
+    
+        // Check if the pressed key is in the possibleKeystrokes array
+        if (possibleKeystrokes.includes(code)) {
+          event.preventDefault();
+          console.log("entered line 64 code", code);
+          // Update player's directionX and directionY based on the key pressed
+          switch (code) {
+            case "ArrowLeft":
+              console.log("code", code);
+              game.player.directionX = -1;
+              break;
+            case "ArrowUp":
+              console.log("code", code);
+              game.player.directionY = -1;
+              break;
+            case "ArrowRight":
+              console.log("code", code);
+              game.player.directionX = 1;
+              break;
+            case "ArrowDown":
+              console.log("code", code);
+              game.player.directionY = 1;
+              break;
+            case "Space":
+                console.log("code", code);
+              jump(game.player);
+               break;
+          }
+        }
+      }
+
+    // Add the handleKeydown function as an event listener for the keydown event
+    window.addEventListener("keydown", handleKeydown);
 
 };
 
