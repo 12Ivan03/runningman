87c24a39f8a8ddcb18eb4addf6b1ef5eb3cb83a3
ttahb <bhattvijay69@hotmail.com>
2023-11-10 17:36:20 +0530

    initialized player object

--- js/player.js
+++ js/player.js
@@ -4,6 +4,7 @@ class Player {
         this.gameScreen = gameScreen;
         this.element = document.createElement('img');
         this.element.src = imgSrc;
+        this.element.alt = "player image";
         this.element.className = "player";
         this.gameScreen.appendChild(this.element);
     }
